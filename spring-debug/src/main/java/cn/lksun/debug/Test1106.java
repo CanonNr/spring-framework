c7ffd739aacb73aa88c42b423b538c2502c16c7c
Canon <717437709@qq.com>
2021-05-21 19:03:42 +0800

    ""

--- spring-debug/src/main/java/cn/lksun/debug/Test1106.java
+++ spring-debug/src/main/java/cn/lksun/debug/Test1106.java
@@ -1,11 +1,17 @@
 package cn.lksun.debug;
 
+import cn.lksun.debug.entity.UserEntity;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test1106 {
 	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml","service.xml");
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-beans.xml");
+		UserEntity user = (UserEntity) ctx.getBean("userEntity");
+		System.out.println("张三");
+		user.name="张三";
+		user.age=15;
+		System.out.println(user.toString());
 	}
 
 }
---
+++ spring-debug/src/main/java/cn/lksun/debug/entity/UserEntity.java
@@ -0,0 +1,16 @@
+package cn.lksun.debug.entity;
+
+public class UserEntity {
+
+	public String name;
+
+	public Integer age;
+
+	@Override
+	public String toString() {
+		return "UserEntity{" +
+				"name='" + name + '\'' +
+				", age=" + age +
+				'}';
+	}
+}
