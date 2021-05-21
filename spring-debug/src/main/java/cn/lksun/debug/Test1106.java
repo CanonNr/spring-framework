aa033116294f9649ab8c112ada4c3ccdd35cdd0e
Canon <717437709@qq.com>
2021-05-21 14:56:07 +0800

    初始化debug模块

--- spring-debug/src/main/java/cn/lksun/debug/Test1106.java
+++ spring-debug/src/main/java/cn/lksun/debug/Test1106.java
@@ -6,10 +6,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test1106 {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml","service.xml");
-
-		Object abb = ctx.getBean("abb");
-		System.out.println("end.....");
-
 	}
 
 }
